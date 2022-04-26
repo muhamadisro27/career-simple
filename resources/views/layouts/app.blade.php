8eeb92031ea83deb0a11d64c55ef5290ae056a2b
muhamadisro27 <mohammadisro2710@gmail.com>
2022-04-26 13:22:23 +0800

    commit changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Mockup Test</title>
+  <title>Career Simple</title>
   @include('partials.style')
 </head>
 <body>
